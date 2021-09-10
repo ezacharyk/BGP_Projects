c0ade6e935815b3ae1cf3bcfe9f06f6720f2783e
E. Zachary Knight <zachary@ezknight.net>
2021-09-10 09:25:53 -0500

    Project Code For BCG Chapter 3

---
+++ Chapter 3: Crop Circles/source/PlayState.hx
@@ -0,0 +1,148 @@
+package;
+
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.FlxState;
+import flixel.math.FlxMath;
+import flixel.math.FlxPoint;
+import flixel.text.FlxText;
+import flixel.util.FlxColor;
+import flixel.util.FlxSpriteUtil;
+
+class PlayState extends FlxState
+{
+	private var text:FlxText;
+
+	// creating counters and sprites for various aspects of the game
+	private var circleTime:Float = 2;
+	private var totalCircles:Int;
+	private var numCircles:Int;
+	private var circle_1:FlxSprite;
+	private var circle_2:FlxSprite;
+	private var line:FlxSprite;
+
+	override public function create()
+	{
+		super.create();
+		FlxG.cameras.bgColor = FlxColor.fromRGBFloat(64, 64, 0, 0);
+		/*
+			This message indicated the start of the crop circles. 
+			In this, we set the text, the location, the size, and the alignment
+			Then we add it to the game state.
+		 */
+		text = new FlxText(0, 0, 640, "Who Created These Crop Circles?");
+		text.size = 16;
+		text.alignment = "center";
+		add(text);
+
+		// Crop Circles are unpredictable. We should not know how many crop circles we get. Create a random number of them
+		totalCircles = Std.int(Math.random() * 20) + 10;
+
+		// A counter so that we know how many circles we have generated
+		numCircles = 0;
+	}
+
+	override public function update(elapsed:Float)
+	{
+		super.update(elapsed);
+
+		// Keep adding crop circles if we are under the randomly generated total
+		if (numCircles < totalCircles)
+		{
+			// this timer tracks when it is time to generate a new circle
+			circleTime -= FlxG.elapsed;
+
+			// if we have reached 0 (or below) generate a cicle
+			if (circleTime < 0)
+			{
+				// Checks if this is the first circle
+				if (numCircles == 0)
+				{
+					// if this is the first circle, we don't need to draw a line. We only need to initialize the first circle variable.
+					circle_1 = new FlxSprite(Std.int(Math.random() * 620), Std.int(Math.random() * 440) + 20);
+
+					// Pass the first circle variable and draw the first circle onto it.
+					drawCropCircle(circle_1);
+				}
+				else
+				{
+					// Creates a new sprte and assigns it to the second circle sprite.
+					circle_2 = new FlxSprite(Std.int(Math.random() * 620), Std.int(Math.random() * 440) + 20);
+
+					// Pass the new circle sprite to the drawCropCircle function
+					drawCropCircle(circle_2);
+
+					// Pass the center cooridinates of both circles to the draCropLine function
+					drawCropLine(circle_1.getGraphicMidpoint(), circle_2.getGraphicMidpoint());
+
+					// Assign the value of the second circle to the first circle variable in preperation for the next circle to draw.
+					circle_1 = circle_2;
+				}
+				// increment the circle counter so we don't exceed the randomly selected number of circles
+				numCircles++;
+
+				// Reset the timer
+				circleTime = 2;
+			}
+		}
+	}
+
+	/**
+	 * Draws a circle onto the provided sprite
+	 *
+	 * @param   FlxSprite   The sprite you want a circle drawn on
+	 *                  
+	 * @return  Void 		This function doesn't return anything
+	 */
+	private function drawCropCircle(circle:FlxSprite):Void
+	{
+		// Randomly creating a radius for the circle We want a minimum of 10 radius and a max of 50.
+		var radius:Int = Std.int(Math.random() * 40) + 10;
+
+		// Create a transparent graphic to act as the canvas for the circle
+		circle.makeGraphic(radius * 2, radius * 2, FlxColor.TRANSPARENT, true);
+
+		// Draw a circle onto the sprite with the radius we picked and positioned centered on the sprite.
+		FlxSpriteUtil.drawCircle(circle, -1, -1, radius, FlxColor.YELLOW);
+
+		// add the new circle to the game state
+		add(circle);
+	}
+
+	/**
+	 * Creates a line sprite and draws a line onto it between two circles
+	 *
+	 * @param   FlxPoint    The center point in the first circle
+	 * @param	FlxPoint	The center point in the second circle
+	 *                  
+	 * @return  Void 		This function doesn't return anything
+	 */
+	private function drawCropLine(circle1:FlxPoint, circle2:FlxPoint):Void
+	{
+		// Create a line style, similar to CSS border styling
+		var lineStyle:LineStyle = {color: FlxColor.YELLOW, thickness: 5};
+
+		// These four lines figure out the extremes so we can figure out the positioning and size of the following sprite
+		var minx:Int = FlxMath.minInt(Std.int(circle1.x), Std.int(circle2.x));
+		var maxx:Int = FlxMath.maxInt(Std.int(circle1.x), Std.int(circle2.x));
+		var miny:Int = FlxMath.minInt(Std.int(circle1.y), Std.int(circle2.y));
+		var maxy:Int = FlxMath.maxInt(Std.int(circle1.y), Std.int(circle2.y));
+
+		// Create a new sprite instance for the line
+		line = new FlxSprite(minx, miny);
+		// Create a transparent graphic to act as a canvas.
+		line.makeGraphic(maxx - minx, maxy - miny, FlxColor.TRANSPARENT, true);
+		if ((circle1.x < circle2.x && circle1.y < circle2.y) || circle1.x > circle2.x && circle1.y > circle2.y)
+		{
+			// One of the circles is in the top left corner. Draw line from top left to bottom right
+			FlxSpriteUtil.drawLine(line, 0, 0, maxx - minx, maxy - miny, lineStyle);
+		}
+		else
+		{
+			// Otherwise, draw the line from the bottom left to top right.
+			FlxSpriteUtil.drawLine(line, 0, maxy - miny, maxx - minx, 0, lineStyle);
+		}
+		// Add the line to the game state
+		add(line);
+	}
+}
