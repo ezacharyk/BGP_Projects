5c61d3ac3868655d0fab70ac17ad810cd288a38c
E. Zachary Knight <zachary@ezknight.net>
2022-05-14 13:10:24 -0500

    Project Code for BGP Chapter 12

---
+++ Chapter 12 Memory 2/source/Main.hx
@@ -0,0 +1,15 @@
+package;
+
+import flixel.FlxGame;
+import openfl.display.Sprite;
+
+class Main extends Sprite
+{
+	public function new()
+	{
+		super();
+		// Adding a width and height to the FlxGame allows us to zoom in, making our smaller sprites bigger.
+		// This is a much better way than I was doing it before.
+		addChild(new FlxGame(320, 180, PlayState));
+	}
+}
