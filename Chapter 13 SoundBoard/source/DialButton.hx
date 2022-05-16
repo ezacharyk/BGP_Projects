5a01a49104c67955fcb7955933ae9acf684e967f
E. Zachary Knight <zachary@ezknight.net>
2022-05-16 08:26:56 -0500

    Quick update to the Soundboard

--- Chapter 13 SoundBoard/source/DialButton.hx
+++ Chapter 13 SoundBoard/source/DialButton.hx
@@ -31,23 +31,31 @@ class DialButton extends FlxSprite
 	{
 		super.update(elapsed);
 		// when the timer hits 0, play a tick, reset the timer, change the frame.
+		// In this, we also play with the panning and volume a bit as the dial rotates.
 		tickTimer -= elapsed;
 		if (tickTimer <= 0)
 		{
-			tickSound.play();
-			tickTimer = 1;
-
 			switch (animation.frameName)
 			{
 				case "dialup":
+					tickSound.volume = .5;
+					tickSound.pan = 1;
 					animation.frameName = "dialright";
 				case "dialright":
+					tickSound.volume = .2;
+					tickSound.pan = 0;
 					animation.frameName = "dialdown";
 				case "dialdown":
+					tickSound.volume = .5;
+					tickSound.pan = -1;
 					animation.frameName = "dialleft";
 				case "dialleft":
+					tickSound.volume = 1;
+					tickSound.pan = 0;
 					animation.frameName = "dialup";
 			}
+			tickSound.play();
+			tickTimer = 1;
 		}
 	}
 }
