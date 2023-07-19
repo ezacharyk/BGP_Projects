541487e60c13dbe01b74275da7aaec58fa6bdf1d
E. Zachary Knight <zachary@ezknight.net>
2023-07-18 21:37:33 -0500

    Setting UFO Movement To Use Preferred Method

--- Chapter 7 UFO 2/source/UFO.hx
+++ Chapter 7 UFO 2/source/UFO.hx
@@ -104,7 +104,7 @@ class UFO extends FlxSprite
 			var newAngle:Float = 0;
 			if (up)
 			{
-				newAngle = 90;
+				newAngle = -90;
 				if (left)
 					newAngle -= 45;
 				else if (right)
@@ -116,7 +116,7 @@ class UFO extends FlxSprite
 			}
 			else if (down)
 			{
-				newAngle = -90;
+				newAngle = 90;
 				if (left)
 					newAngle += 45;
 				else if (right)
@@ -125,18 +125,17 @@ class UFO extends FlxSprite
 			}
 			else if (left)
 			{
-				newAngle = 0;
+				newAngle = 180;
 				facing = LEFT;
 			}
 			else if (right)
 			{
-				newAngle = 180;
+				newAngle = 0;
 				facing = RIGHT;
 			}
 
 			// we determine our velocity based on angle and speed
-			velocity.set(SPEED, 0);
-			velocity.pivotDegrees(FlxPoint.weak(0, 0), newAngle);
+			velocity.setPolarDegrees(SPEED, newAngle);
 		}
 		else
 		{
